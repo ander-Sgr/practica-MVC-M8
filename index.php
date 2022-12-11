5b328dcf17831008c1d7d03004058157c492f7a1
Anderson Estrella <andersonbarreto2001@gmail.com>
2022-12-11 12:46:43 +0100

    he modificar por un swich

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
 </head>
+
 <body>
     <?php
-        include_once("./controller/ControllerStudent.php");
-        $controlador = new ControllerStudent();
-        $controlador->showStudents();
+    include_once("./controller/ControllerStudent.php");
+    if (isset($_REQUEST['action'])) {
+        $method = $_REQUEST['action'];
+        if (method_exists($controller, $method)) {
+            $controller->{$method}();
+
+        }
+    } else {
+        $controller->showStudents();
+    }
     ?>
 </body>
+
 </html>
